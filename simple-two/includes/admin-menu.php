53f1c5b030c23dd59b7780e2208c8ae8c3dc4ab0
SamSkull19 <sifatsamin19@gmail.com>
2026-04-23 12:59:49 +0600

    Simple Two Pluing Admin Submenu

--- simple-two/includes/admin-menu.php
+++ simple-two/includes/admin-menu.php
@@ -15,9 +15,44 @@ class Admin_Menu {
             'dashicons-media-spreadsheet',
             50
         );
+
+        add_submenu_page(
+            'simple-two',
+            'Dashboard',
+            'Dashboard',
+            'manage_options',
+            'simple-two',
+            [$this, 'simple_two_content']
+        );
+        
+        add_submenu_page(
+            'simple-two',
+            'Settings',
+            'Settings',
+            'manage_options',
+            'simple-two-settings',
+            [$this, 'simple_two_settings']
+        );
+
+        add_submenu_page(
+            'simple-two',
+            'About Page',
+            'About Page',
+            'manage_options',
+            'simple-two-about',
+            [$this, 'simple_two_about']
+        );
     }
 
     public function simple_two_content() {
         include_once plugin_dir_path(__FILE__) . 'simple-two-contents.php';
+    }
+    
+    public function simple_two_settings() {
+        echo "<div class='wrap'><h1>Settings Page</h1></div>";
+    }
+
+    public function simple_two_about() {
+        echo "<div class='wrap'><h1>About Page</h1></div>";
     }
 }
