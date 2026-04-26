d2180d890389feb508db54fb44159ba0cd83a5e2
SamSkull19 <sifatsamin19@gmail.com>
2026-04-26 09:55:10 +0600

    Admin Column Tested

--- admin-column/includes/Column-Test.php
+++ admin-column/includes/Column-Test.php
@@ -1,7 +1,15 @@
 <?php
 
+defined('ABSPATH') or exit;
+
 class COLUMN_TEST {
     public function __construct() {
-        echo 'Column Test';
+        add_filter('manage_posts_columns', [$this, 'add_custom_column']);
+    }
+
+    public function add_custom_column($columns) {
+        $columns['price'] = 'Price';
+
+        return $columns;
     }
 }
